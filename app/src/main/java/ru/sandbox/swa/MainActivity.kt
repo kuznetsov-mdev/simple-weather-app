bcb4427766c561d4cb2e45c2a50709b3a6f6d491
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-21 00:19:17 +0500

    swa-theme: add center position for Hello world text in MainActivity

--- app/src/main/java/ru/sandbox/swa/MainActivity.kt
+++ app/src/main/java/ru/sandbox/swa/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.compose.SimpleWeatherAppTheme
 
@@ -18,7 +21,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleWeatherAppTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Hello world")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Hello world")
+                    }
+
                 }
             }
         }
