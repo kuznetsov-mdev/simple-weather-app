8ac3740706ab2c9f5dfedaf437ec1877974e7652
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-20 13:30:50 +0500

    build-logic: fix COMPILE_SDK version to 35

--- build-logic/src/main/kotlin/util/AndroidConst.kt
+++ build-logic/src/main/kotlin/util/AndroidConst.kt
@@ -2,7 +2,7 @@ import org.gradle.api.JavaVersion
 
 object AndroidConst {
     const val NAMESPACE = "ru.sandbox.swa"
-    const val COMPILE_SKD = 33
+    const val COMPILE_SKD = 35
     const val MIN_SKD = 29
     val COMPILE_JDK_VERSION = JavaVersion.VERSION_11
     const val KOTLIN_JVM_TARGET = "11"
