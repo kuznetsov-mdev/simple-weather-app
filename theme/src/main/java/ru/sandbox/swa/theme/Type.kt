f0f9cc8e1ef74b4e401e89228d1f201cabbcfd12
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-20 23:21:38 +0500

    swa-theme: add theme files generated by Material Theme Builder

---
+++ theme/src/main/java/ru/sandbox/swa/theme/Type.kt
@@ -0,0 +1,27 @@
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.LineHeightStyle
+import ru.sandbox.swa.theme.R
+
+private val poppins = FontFamily(
+    Font(R.font.poppins_regular),
+    Font(R.font.poppins_bold),
+    Font(R.font.poppins_extra_bold),
+    Font(R.font.poppins_medium),
+    Font(R.font.poppins_thin),
+    Font(R.font.poppins_italic),
+    Font(R.font.poppins_light),
+    Font(R.font.poppins_black),
+)
+val defaultTextStyLe = TextStyle(
+    fontFamily = poppins,
+    platformStyle = PlatformTextStyle(
+        includeFontPadding = false,
+    ),
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None,
+    ),
+)
