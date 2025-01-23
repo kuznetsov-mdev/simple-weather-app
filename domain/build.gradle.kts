cb50cf94655da0ec7c03960ce00cb1a9b1d06af3
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-23 13:12:23 +0500

    build-logic: hot-fix move jvmToolchain from domain build.gradle.kts to jvm-convention.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     id("jvm-convention")
 }
 
-kotlin {
-    jvmToolchain(AndroidConst.JAVA_VERSION)
-}
-
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
