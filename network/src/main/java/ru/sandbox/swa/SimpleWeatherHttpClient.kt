ba97f91867b226ead228eb0eb08e6937a38970da
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-24 17:07:44 +0500

    swa-network: add http client for network call

---
+++ network/src/main/java/ru/sandbox/swa/SimpleWeatherHttpClient.kt
@@ -0,0 +1,24 @@
+package ru.sandbox.swa
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object SimpleWeatherHttpClient {
+    private const val BASE_URL = "http://api.openweathermap.org"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(httpClient)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+}
