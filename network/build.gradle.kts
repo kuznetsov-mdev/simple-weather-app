fd310732abefe174c63fc522b89d062a7d415f8b
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-24 16:54:55 +0500

    swa-network: add dependencies

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     id("jvm-convention")
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.devtools.ksp)
+}
+
+dependencies {
+    //Kotlinx serialization
+    implementation(libs.kotlinx.serialization.json)
+    //OkHttp
+    implementation(libs.logging.interceptor)
+    //Moshi
+    implementation(libs.moshi.moshi)
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.codegen.ksp)
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
 }
