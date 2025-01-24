16d43de45d8b4b510af51c9c436ff430efa91643
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-24 17:13:50 +0500

    swa-network: add dtos

---
+++ network/src/main/java/ru/sandbox/swa/dto/forecast/CloudsDto.kt
@@ -0,0 +1,8 @@
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class CloudsDto(
+    @Json(name = "all")
+    val all: Int
+)
---
+++ network/src/main/java/ru/sandbox/swa/dto/forecast/WeatherDto.kt
@@ -0,0 +1,18 @@
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class WeatherDto(
+    @Json(name = "clouds")
+    val clouds: CloudsDto,
+    @Json(name = "dt")
+    val dateTimeMs: Int,
+    @Json(name = "dt_txt")
+    val date: String,
+    @Json(name = "main")
+    val commonInfo: CommonInfoDto,
+    @Json(name = "rain")
+    val rain: RainDto?,
+    @Json(name = "wind")
+    val wind: WindDto
+)
