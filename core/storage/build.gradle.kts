5162171742854b8497201b0c00935648a6ce69f9
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-27 15:10:44 +0500

    swa-storage: add dependencies on serialization and room

--- core/storage/build.gradle.kts
+++ core/storage/build.gradle.kts
@@ -1,3 +1,14 @@
 plugins {
     id("android-lib-convention")
+    alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+dependencies {
+    //Kotlinx serialization
+    implementation(libs.kotlinx.serialization.json)
+    //Room
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 }
