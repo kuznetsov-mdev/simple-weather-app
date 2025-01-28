16a39ee59a9a52f34b7203aa02cd10d20c68b705
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-28 16:51:02 +0500

    swa-service: add hilt dependencies

--- core/service/build.gradle.kts
+++ core/service/build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     id("android-lib-convention")
+    alias(libs.plugins.devtools.ksp)
+}
+
+dependencies {
+    //Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
