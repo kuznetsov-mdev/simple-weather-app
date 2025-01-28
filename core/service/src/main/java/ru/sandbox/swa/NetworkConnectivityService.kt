a1f1726a053861417cfd59beb0b6df8e6b542e6b
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-28 17:11:27 +0500

    swa-service: add ConnectivityService implementation

--- core/service/src/main/java/ru/sandbox/swa/NetworkConnectivityService.kt
+++ core/service/src/main/java/ru/sandbox/swa/NetworkConnectivityService.kt
@@ -1,4 +1,58 @@
 package ru.sandbox.swa
 
-class NetworkConnectivityService {
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import ru.sandbox.swa.api.ConnectivityServiceApi
+import javax.inject.Inject
+
+class NetworkConnectivityService @Inject constructor(
+    private val context: Application
+) : ConnectivityServiceApi {
+    private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    override fun observe(): Flow<ConnectivityServiceApi.Status> {
+        return callbackFlow {
+            val callback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    launch { send(ConnectivityServiceApi.Status.Available) }
+                }
+
+                override fun onLosing(network: Network, maxMsToLive: Int) {
+                    super.onLosing(network, maxMsToLive)
+                    launch { send(ConnectivityServiceApi.Status.Losing) }
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    launch { send(ConnectivityServiceApi.Status.Lost) }
+                }
+
+                override fun onUnavailable() {
+                    super.onUnavailable()
+                    launch { send(ConnectivityServiceApi.Status.Unavailable) }
+                }
+            }
+
+            connectivityManager.registerDefaultNetworkCallback(callback)
+
+            awaitClose {
+                connectivityManager.unregisterNetworkCallback(callback)
+            }
+        }.distinctUntilChanged()
+
+    }
+
+    override fun isNetworkAvailable(): Boolean {
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return networkCapabilities != null && networkCapabilities.hasCapability(NET_CAPABILITY_INTERNET)
+    }
 }
