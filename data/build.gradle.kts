8a5f3ccd59a2c46098aeb5f1564b2d0464128e8a
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-01-31 16:11:42 +0500

    swa-data: add dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     id("android-lib-convention")
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.devtools.ksp)
+
 }
 
 dependencies {
+    implementation(projects.domain)
+    implementation(projects.core.network)
+    implementation(projects.core.storage)
+    implementation(projects.core.service)
 
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
