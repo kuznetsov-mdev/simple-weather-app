5f73f59d4e98eae18793874583fffd1635febb28
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-02-06 16:24:56 +0500

    swa-presentation: add Timber import

--- app/src/main/java/ru/sandbox/swa/SimpleWeatherApp.kt
+++ app/src/main/java/ru/sandbox/swa/SimpleWeatherApp.kt
@@ -1,11 +1,11 @@
 package ru.sandbox.swa
 
 import android.app.Application
+import timber.log.Timber
 
 class SimpleWeatherApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        SoLoader.init(this, false)
     }
 }
