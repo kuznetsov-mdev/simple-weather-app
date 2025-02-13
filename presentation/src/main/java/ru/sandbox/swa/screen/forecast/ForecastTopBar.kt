73b52ad31281cea3826bd9897106b0b3a862b2c4
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-02-13 10:29:39 +0500

    swa-presentation: add Forecast MVVM

---
+++ presentation/src/main/java/ru/sandbox/swa/screen/forecast/ForecastTopBar.kt
@@ -0,0 +1,56 @@
+package ru.sandbox.swa.screen.forecast
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import ru.sandbox.swa.R
+import ru.sandbox.swa.SimpleWeatherAppTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ForecastTopBar(
+    onBackClick: () -> Unit
+) {
+    TopAppBar(
+        title = {},
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = Color.Transparent,
+            actionIconContentColor = MaterialTheme.colorScheme.scrim,
+            navigationIconContentColor = MaterialTheme.colorScheme.primary
+        ),
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_back),
+                    contentDescription = null
+                )
+            }
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun ForecastTopBarPreview() {
+    SimpleWeatherAppTheme {
+        Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
+            ForecastTopBar(
+                onBackClick = {}
+            )
+        }
+    }
+}
---
+++ presentation/src/main/java/ru/sandbox/swa/screen/forecast/ForecastViewModel.kt
@@ -0,0 +1,35 @@
+package ru.sandbox.swa.screen.forecast
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.sandbox.swa.model.WeatherItem
+import ru.sandbox.swa.usecase.GetWeatherForecastUseCase
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltViewModel
+class ForecastViewModel @Inject constructor(
+    @Named("openWeatherApiKey")
+    private val apiKey: String,
+    private val getWeatherForecastUseCase: GetWeatherForecastUseCase,
+) : ViewModel() {
+    private val _isDataLoading = MutableStateFlow(true)
+    val isDataLoading: StateFlow<Boolean>
+        get() = _isDataLoading.asStateFlow()
+
+    private val _forecast = MutableStateFlow<List<WeatherItem>>(emptyList())
+    val forecast: StateFlow<List<WeatherItem>>
+        get() = _forecast.asStateFlow()
+
+    fun getForecast(lat: String, lon: String) {
+        viewModelScope.launch {
+            _forecast.value = getWeatherForecastUseCase(lat = lat, lon = lon, apiKey)
+            _isDataLoading.value = false
+        }
+    }
+}
