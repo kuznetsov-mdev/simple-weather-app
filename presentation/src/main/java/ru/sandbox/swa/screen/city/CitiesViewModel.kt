ba562b368a76925be56361b9876bfd1423385777
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-02-13 10:29:18 +0500

    swa-presentation: add Cities MVVM

---
+++ presentation/src/main/java/ru/sandbox/swa/screen/city/CitiesViewModel.kt
@@ -0,0 +1,32 @@
+package ru.sandbox.swa.screen.city
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import ru.sandbox.swa.model.CityItem
+import ru.sandbox.swa.usecase.GetSearchHistoryUseCase
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltViewModel
+class CitiesViewModel @Inject constructor(
+    @Named("openWeatherApiKey")
+    private val apiKey: String,
+    private val getSearchHistoryUseCase: GetSearchHistoryUseCase
+) : ViewModel() {
+
+    private val _searchedCities = MutableStateFlow<List<CityItem>>(emptyList())
+    val searchedCities: StateFlow<List<CityItem>>
+        get() = _searchedCities.asStateFlow()
+
+    fun fetchCities() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _searchedCities.value = getSearchHistoryUseCase(apiKey)
+        }
+    }
+}
