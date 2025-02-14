5ade1383ffa056240e556456a61520d050ab9b5c
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-02-14 22:38:33 +0500

    swa-storage: fix WeatherForecastDto (add JsonClass annotation)

--- core/network/src/main/java/ru/sandbox/swa/dto/WeatherForecastDto.kt
+++ core/network/src/main/java/ru/sandbox/swa/dto/WeatherForecastDto.kt
@@ -2,7 +2,9 @@ package ru.sandbox.swa.dto
 
 import WeatherDto
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class WeatherForecastDto(
     @Json(name = "list")
     val forecast: List<WeatherDto>
